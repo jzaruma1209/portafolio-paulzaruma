497a8581f9b699bb93eac924e088d4f0653b153a
Tu Nombre <jzaruma1209@gmail.com>
2024-11-13 12:36:44 -0500

    Versión 1.2 del portafolio

---
+++ assets/js/components/language.js
@@ -0,0 +1,16 @@
+// components/language.js
+const langToggle = document.getElementById('language-toggle');
+const currentLang = window.location.href.includes('index-en.html');
+langToggle.checked = currentLang;
+
+function translator() {
+    langToggle.addEventListener('change', () => {
+        if (langToggle.checked) {
+            window.location.href = 'index-en.html';
+        } else {
+            window.location.href = 'index.html';
+        }
+    });
+}
+
+export default translator;
---
+++ assets/js/main.js
@@ -0,0 +1,21 @@
+import parallax from './components/parallax.js';
+import activeMenu from './components/selected_menu.js';
+import updateCopyrightYear from './helpers/date_updater.js';
+import loader from './helpers/loader.js';
+import resetToHome from './helpers/reload_page.js';
+import sendEmail from './helpers/send_form.js';
+import translator from './components/language.js';
+
+loader();
+
+parallax();
+
+activeMenu();
+
+resetToHome();
+
+updateCopyrightYear();
+
+sendEmail();
+
+translator();
